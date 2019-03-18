8190e71e520e2a8404f9c1e8a6b55c6d8060958b
Ruanderson Vieira <ruanderson.vieira@gmail.com>
2019-03-17 22:05:17 -0300

    final function event update crash

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const axios = require('axios');
 const redis = require('redis');
+
+// Base url for api redis
+baseUrl = 'http://127.0.0.1:8007/';
+
+// Events Config
 let events = require('events');
 let eventEmitter = new events.EventEmitter();
 
-let updateCrash = function ()
-{
-  console.log('Update Crash Status');
-}
-
-eventEmitter.on('update', updateCrash);
-
+// Array to get "inativo" crash status
 let newData = [];
 
-
-baseUrl = 'http://127.0.0.1:8007';
-
+// Get all crash in "inativo" status and set in newData
 getStatusoff();
 
 function getStatusoff(){
@@ -24,9 +21,13 @@ function getStatusoff(){
                 for(i=0; i < response.data.length; i++){
                     if(response.data[i].status=="Inativo"){
                         newData.push(response.data[i]);
-                    }
-                }
+                    };
+                };
+                if(newData[0] == null){
+                    return console.log("Todos carros Ativos!");
+                } else{
                 redisSave(newData);
+                };
             };
         })
         .catch(error => {
@@ -34,20 +35,22 @@ function getStatusoff(){
         })
 }
 
+// Redi config
 let redisClient = redis.createClient({ host: 'redis-16564.c73.us-east-1-2.ec2.cloud.redislabs.com', port: 16564 });
 redisClient.auth('XUBIIoEhmZiHwhwq81LIbn3C6Tw0hzo8', function (err, reply){});
 
-
+//Redis Save function
 function redisSave(docs) {
     redisClient.set("carinativo", JSON.stringify(docs), function (err, result) {
       if (err) return console.log(err);
          if(result == "OK"){
-             console.log("passo 1")
+             console.log("Salvo 'Inativo' no redis")
             executeEvent(getRandomTime());
          };
         });
-    };
+};
 
+// Get random time function
 function getRandomTime() {
     let min = 30000;
     let max = 60000;
@@ -56,6 +59,7 @@ function getRandomTime() {
     return Math.floor(Math.random() * (max - min)) + min;
 };
 
+// Execute a event
 function executeEvent(time){
     setTimeout(function(){ 
         console.log(time); 
@@ -63,5 +67,21 @@ function executeEvent(time){
         executeEvent(getRandomTime());
     }, time);
 
-}
+};
 
+// Update event
+let updateCrash = function (){
+    axios.put(baseUrl + newData[0]._id,{
+        name: newData[0].name,
+        status: "Ativo"
+    })
+        .then(response => {
+            console.log(response.data);
+            getStatusoff();
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    console.log('Update Crash' + newData[0]._id +' Status');
+  };
+  eventEmitter.on('update', updateCrash);
